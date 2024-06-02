02de10c136fab6f3a5433eba8661eaa9a3a1a2ff
Viktor Shvidkiy <vikt.sh@gmail.com>
2024-06-02 16:15:15 +0300

    Initial commit

---
+++ src/test/java/com/example/zlatopolsky_spring/ZlatopolskySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zlatopolsky_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZlatopolskySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
