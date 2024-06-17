91f0b1780fa34e88d10ddd54791e656d7c9d5f5c
Viktor Shvidkiy <vikt.sh@gmail.com>
2024-06-17 19:48:52 +0300

    1. Выполнил задания до 2.1

---
+++ src/main/java/com/example/zlatopolsky_spring/controller/Chapter2Controller.java
@@ -0,0 +1,36 @@
+package com.example.zlatopolsky_spring.controller;
+
+import com.example.zlatopolsky_spring.services.Chapter2Service;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/chapter2")
+public class Chapter2Controller {
+    private final Chapter2Service chapter2Service;
+
+    public Chapter2Controller(Chapter2Service chapter2Service) {
+        this.chapter2Service = chapter2Service;
+    }
+
+
+    @GetMapping("/lesson1")
+    public String getLesson17Page(){
+        return "chapter2/2.1";
+    }
+
+    @PostMapping("/lesson1")
+    public String getLesson17ResultPage(Model model,
+                                        @RequestParam("a") Integer a,
+                                        @RequestParam("x") Integer x){
+        model.addAttribute("a", a);
+        model.addAttribute("x", x);
+        model.addAttribute("ya", chapter2Service.getLesson1aResult(x));
+        model.addAttribute("yb", chapter2Service.getLesson1bResult(a));
+        return "chapter2/2.1_result";
+    }
+}
---
+++ src/main/java/com/example/zlatopolsky_spring/services/Chapter2Service.java
@@ -0,0 +1,14 @@
+package com.example.zlatopolsky_spring.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Chapter2Service {
+
+    public Integer getLesson1aResult(Integer x) {
+        return 17*(x*x)-6*x+13;
+    }
+    public Integer getLesson1bResult(Integer a) {
+        return 3*(a*a)+5*a-21;
+    }
+}
